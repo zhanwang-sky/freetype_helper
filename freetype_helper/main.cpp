748bac4bedf99c5bb90378e1ae3c31d6b208142e
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2024-08-02 15:23:29 +0800

    Initial Commit

---
+++ freetype_helper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  freetype_helper
+//
+//  Created by zhanwang-sky on 2024/8/2.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
